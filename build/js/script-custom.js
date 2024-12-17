5d46483d39fdaf360a4cb414e4e9615138730b2d
Alfredo  Recinos <42593596+AlfredoRecinos@users.noreply.github.com>
2024-12-16 22:48:41 -0600

    Update script-custom.js

--- build/js/script-custom.js
+++ build/js/script-custom.js
@@ -56,7 +56,7 @@ window.addEventListener('scroll', scrollUp)
 
 // Texto animado con JS
 var typed = new Typed(".multiple-text", {
-  strings: [ "Graphic Designer", "UI Designer", "Frontend Developer" ],
+  strings: [ "UX/UI Designer","Advertising Designer","Frontend Developer" ],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
